88e2d424ce2be76e2cba1a4f9f75206e5695536b
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2023-12-28 04:01:50 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_RMX2155L1.mk
+    $(LOCAL_DIR)/twrp_ossi.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RMX2155L1-user \
-    twrp_RMX2155L1-userdebug \
-    twrp_RMX2155L1-eng
+    twrp_ossi-user \
+    twrp_-ossi-userdebug \
+    twrp_ossi-eng
