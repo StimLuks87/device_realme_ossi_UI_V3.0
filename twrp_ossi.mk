aeeecfa64a875b9ba9d533a054b92bf1343a9145
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2023-12-28 04:06:42 +0500

    Update and rename twrp_RMX2155L1.mk to twrp_ossi.mk

--- twrp_ossi.mk
+++ twrp_ossi.mk
@@ -12,13 +12,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from RMX2155L1 device
-$(call inherit-product, device/realme/RMX2155L1/device.mk)
+# Inherit from ossi device
+$(call inherit-product, device/realme/ossi/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
@@ -29,16 +30,19 @@ $(call inherit-product-if-exists, vendor/pb/config/common.mk)
 # Inherit from TWRP-common stuff, if building TWRP.
 $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
 
-PRODUCT_DEVICE := RMX2155L1
 PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := RMX2155
+PRODUCT_DEVICE := ossi
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
-PRODUCT_RELEASE_NAME := realme 7
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := realme 7	
+BOARD_VENDOR := realme
+TARGET_VENDOR := realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2155L1 \
+    PRODUCT_NAME=RMX2155 \
     PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1668670161293 release-keys"
 
 BUILD_FINGERPRINT := realme/RMX2155EEA/RMX2155L1:12/SP1A.210812.016/Q.GDPR.bf75e7-1:user/release-keys
