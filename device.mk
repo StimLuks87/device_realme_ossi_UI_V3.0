81a70906fc8365b6ae22c2ade40d227178c88618
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2024-01-05 02:19:42 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 
 LOCAL_PATH := device/realme/ossi
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
