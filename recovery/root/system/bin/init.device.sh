ee015ba9f8cd6a32904243c9bc69e092cb68c3d7
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2024-01-04 23:14:47 +0500

    Update init.device.sh

--- recovery/root/system/bin/init.device.sh
+++ recovery/root/system/bin/init.device.sh
@@ -7,11 +7,13 @@ echo $prjName
 
 case $prjName in
     "20682")
-        resetprop "ro.product.model" "realme 7"
+        resetprop "ro.product.model" "Realme 7"
         resetprop "ro.product.name" "RMX2155"
-        resetprop "ro.build.product" "RMX2155L1"
         resetprop "ro.product.device" "RMX2155L1"
         ;;
+        resetprop "ro.product.model" "Realme 7"
+        resetprop "ro.product.name" "RMX2151"
+        resetprop "ro.product.device" "RMX2151L1"
     *)
 esac
 
